ac87f80c7d06aa5162d8a4a79684125b73a90054
samueldeschamps@gmail.com <samueldeschamps@gmail.com@e197c4ee-6bdd-a554-de2f-b516fb6a7f70>
2014-06-07 00:34:18 +0000

    - Adicionando testes de Feriado; - Criando suite de testes.

---
+++ ponto-test/src/br/udesc/ads/ponto/entidades/TestFeriadoPersistence.java
@@ -0,0 +1,24 @@
+package br.udesc.ads.ponto.entidades;
+
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestFeriadoPersistence extends PersistenceTest {
+
+	@Test
+	public void testInsereRecuperaFeriado() {
+		Feriado natal = new Feriado();
+		natal.setData(new LocalDate(2014, 12, 25));
+		natal.setNome("Natal");
+
+		Assert.assertNull(natal.getId());
+		entityManager.persist(natal);
+		Assert.assertNotNull(natal.getId());
+
+		Feriado actual = entityManager.find(Feriado.class, natal.getId());
+		Assert.assertEquals("Natal", actual.getNome());
+		Assert.assertEquals(new LocalDate(2014, 12, 25), actual.getData());
+	}
+
+}
