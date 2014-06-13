9c92371c3a2da4d1ef13bf0bdeb44a9353ffbaff
anderson.dkfachini@gmail.com <anderson.dkfachini@gmail.com@e197c4ee-6bdd-a554-de2f-b516fb6a7f70>
2014-06-13 22:39:54 +0000

    Implementações da tela de importação de colaboradores

--- trunk/ponto/src/br/udesc/ads/ponto/web/ImportarColaboradoresController.java
+++ trunk/ponto/src/br/udesc/ads/ponto/web/ImportarColaboradoresController.java
@@ -7,6 +7,10 @@ import org.icefaces.ace.component.fileentry.FileEntry;
 import org.icefaces.ace.component.fileentry.FileEntryEvent;
 import org.icefaces.ace.component.fileentry.FileEntryResults;
 
+import br.udesc.ads.ponto.servicos.ColaboradorService;
+import br.udesc.ads.ponto.util.JsfUtils;
+import br.udesc.ads.ponto.util.Messages;
+
 @ManagedBean(name = "importarColaboradoresController")
 @ViewScoped
 public class ImportarColaboradoresController {
@@ -14,11 +18,12 @@ public class ImportarColaboradoresController {
 	public void fileListener(FileEntryEvent e) {
 		FileEntry fe = (FileEntry) e.getComponent();
 		FileEntryResults results = fe.getResults();
+		ColaboradorService cs = new ColaboradorService();
 		
 		for (FileEntryResults.FileInfo i : results.getFiles()) {
-			System.out.println(i.getFileName());
-//			i.getFile();
-			
+			cs.importarColaboradores(i.getFile());
 		}
+		
+		JsfUtils.addMensagemInfo(Messages.getString("msgColaboradoresImportadosSucesso"));
 	}
 }
