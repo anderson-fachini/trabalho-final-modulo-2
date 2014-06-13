2c884c25b194e92ea3e9fa52431fb7fb25ce3220
samueldeschamps@gmail.com <samueldeschamps@gmail.com@e197c4ee-6bdd-a554-de2f-b516fb6a7f70>
2014-06-13 20:07:39 +0000

    Interface de importação de colaboradores.

---
+++ ponto/src/br/udesc/ads/ponto/servicos/ColaboradorService.java
@@ -0,0 +1,19 @@
+package br.udesc.ads.ponto.servicos;
+
+import java.io.File;
+
+public class ColaboradorService {
+
+	/**
+	 * Realiza a importação do arquivo de colaboradores para a base de dados.
+	 * 
+	 * @param arquivo
+	 *            O arquivo a ser importado.
+	 */
+	public void importarColaboradores(File arquivo) {
+
+		System.out.println("Importando arquivo '" + arquivo.getName() + "'...");
+		// TODO Em implementação (Samuel)...
+	}
+
+}
