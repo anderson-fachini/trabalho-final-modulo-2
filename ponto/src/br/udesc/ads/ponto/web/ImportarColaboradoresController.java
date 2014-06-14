873b0f99e7aa2a46c95f9131380e8196acd67df3
anderson.dkfachini@gmail.com <anderson.dkfachini@gmail.com@e197c4ee-6bdd-a554-de2f-b516fb6a7f70>
2014-06-14 00:00:16 +0000

    Criado arquivo para substituir as mensagens padrão do ICEfaces. Alteradas as mensagens padrão de sucesso no upload de arquivos.

--- ponto/src/br/udesc/ads/ponto/web/ImportarColaboradoresController.java
+++ ponto/src/br/udesc/ads/ponto/web/ImportarColaboradoresController.java
@@ -1,5 +1,7 @@
 package br.udesc.ads.ponto.web;
 
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -13,8 +15,14 @@ import br.udesc.ads.ponto.util.Messages;
 
 @ManagedBean(name = "importarColaboradoresController")
 @ViewScoped
-public class ImportarColaboradoresController {
+public class ImportarColaboradoresController implements Serializable { 
 	
+	private static final long serialVersionUID = -7072318450107718147L;
+
+	/**
+	 * Método chamado ao submeter o arquivo de importação de colaboradores
+	 * @param e
+	 */
 	public void fileListener(FileEntryEvent e) {
 		FileEntry fe = (FileEntry) e.getComponent();
 		FileEntryResults results = fe.getResults();
