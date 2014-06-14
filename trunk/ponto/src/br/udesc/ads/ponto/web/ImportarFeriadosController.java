3eb3579ce21de080a2720e5840392b4a9f7b4359
anderson.dkfachini@gmail.com <anderson.dkfachini@gmail.com@e197c4ee-6bdd-a554-de2f-b516fb6a7f70>
2014-06-14 16:34:15 +0000

    Criada tela de importação de feriados

---
+++ trunk/ponto/src/br/udesc/ads/ponto/web/ImportarFeriadosController.java
@@ -0,0 +1,36 @@
+package br.udesc.ads.ponto.web;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import org.icefaces.ace.component.fileentry.FileEntry;
+import org.icefaces.ace.component.fileentry.FileEntryEvent;
+import org.icefaces.ace.component.fileentry.FileEntryResults;
+
+import br.udesc.ads.ponto.util.JsfUtils;
+import br.udesc.ads.ponto.util.Messages;
+
+@ManagedBean(name = "importarFeriadosController")
+@ViewScoped
+public class ImportarFeriadosController implements Serializable { 
+
+	private static final long serialVersionUID = 6795388206141407679L;
+
+	/**
+	 * Método chamado ao submeter o arquivo de importação de feriados
+	 * @param e
+	 */
+	public void fileListener(FileEntryEvent e) {
+		FileEntry fe = (FileEntry) e.getComponent();
+		FileEntryResults results = fe.getResults();
+		
+		for (FileEntryResults.FileInfo i : results.getFiles()) {
+//			TODO ColaboradorService.get().importarColaboradores(i.getFile());
+			System.out.println(i.getFileName());
+		}
+		
+		JsfUtils.addMensagemInfo(Messages.getString("msgColaboradoresImportadosSucesso"));
+	}
+}
