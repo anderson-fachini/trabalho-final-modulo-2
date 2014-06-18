a6826399866e525f1bbccd5a7e107ee26b5f09df
samueldeschamps@gmail.com <samueldeschamps@gmail.com@e197c4ee-6bdd-a554-de2f-b516fb6a7f70>
2014-06-18 00:02:11 +0000

    Implementando os testes de apuração de ponto (falta bastante ainda).

--- ponto-test/src/br/udesc/ads/ponto/servicos/ApuradorMarcacoesTest.java
+++ ponto-test/src/br/udesc/ads/ponto/servicos/ApuradorMarcacoesTest.java
@@ -1,5 +1,7 @@
 package br.udesc.ads.ponto.servicos;
 
+import java.util.Arrays;
+
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
 import org.junit.Assert;
@@ -9,6 +11,7 @@ import org.junit.Test;
 import br.udesc.ads.ponto.entidades.Apuracao;
 import br.udesc.ads.ponto.entidades.Colaborador;
 import br.udesc.ads.ponto.entidades.Marcacao;
+import br.udesc.ads.ponto.entidades.Ocorrencia;
 import br.udesc.ads.ponto.entidades.Situacao;
 
 public class ApuradorMarcacoesTest {
@@ -32,13 +35,21 @@ public class ApuradorMarcacoesTest {
 		return result;
 	}
 
+	private void assertOcorrencias(Ocorrencia[] ocorrencias, Apuracao apuracao) {
+		Ocorrencia[] actual = new Ocorrencia[apuracao.getOcorrenciasSize()];
+		for (int i = 0; i < actual.length; ++i) {
+			actual[i] = apuracao.getOcorrencia(i);
+		}
+		Assert.assertEquals(Arrays.toString(ocorrencias), Arrays.toString(actual));
+	}
+
 	// TODO Verificar se é possível mockear Feriados, Config e outras apurações
 	// para não precisar consultar no banco
 
 	// Cenários Samuel:
-	
+
 	@Test
-	public void testApuracao_Cenario1_NenhumaOcorrencia() {
+	public void testApuracao_1_NenhumaOcorrencia() {
 		Apuracao apuracao = new Apuracao();
 		apuracao.setColaborador(colaborador);
 		apuracao.setData(new LocalDate(2014, 2, 6));
@@ -50,7 +61,7 @@ public class ApuradorMarcacoesTest {
 		apurador.processarApuracao(apuracao);
 
 		Assert.assertEquals(true, apuracao.getApurada());
-		Assert.assertEquals(0, apuracao.getOcorrenciasSize());
+		assertOcorrencias(new Ocorrencia[] {}, apuracao);
 		Assert.assertEquals(new LocalTime(8, 0, 0), apuracao.getHorasTrabalhadas());
 		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasExcedentes());
 		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasFaltantes());
@@ -59,7 +70,126 @@ public class ApuradorMarcacoesTest {
 		Assert.assertNull(apuracao.getDataAprovacao());
 		Assert.assertNull(apuracao.getResponsavelAprovacao());
 	}
-	
+
+	// TODO Este cenário deveria ter outro nome
+	@Test
+	public void testApuracao_2_OcorrenciaMarcacoesForaPadrao_1() {
+		Apuracao apuracao = new Apuracao();
+		apuracao.setColaborador(colaborador);
+		apuracao.setData(new LocalDate(2014, 2, 6));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(8, 5, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(12, 5, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(13, 5, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(17, 5, 0)));
+
+		apurador.processarApuracao(apuracao);
+
+		Assert.assertEquals(true, apuracao.getApurada());
+		assertOcorrencias(new Ocorrencia[] {}, apuracao);
+		Assert.assertEquals(new LocalTime(8, 0, 0), apuracao.getHorasTrabalhadas());
+		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasExcedentes());
+		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasFaltantes());
+		// Não deve ter ocorrido nenhuma aprovação:
+		Assert.assertNull(apuracao.getHorasAbonadas());
+		Assert.assertNull(apuracao.getDataAprovacao());
+		Assert.assertNull(apuracao.getResponsavelAprovacao());
+	}
+
+	// TODO Este cenário deveria ter outro nome
+	@Test
+	public void testApuracao_3_OcorrenciaMarcacoesForaPadrao_2() {
+		Apuracao apuracao = new Apuracao();
+		apuracao.setColaborador(colaborador);
+		apuracao.setData(new LocalDate(2014, 2, 6));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(7, 55, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(11, 55, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(12, 55, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(16, 55, 0)));
+
+		apurador.processarApuracao(apuracao);
+
+		Assert.assertEquals(true, apuracao.getApurada());
+		assertOcorrencias(new Ocorrencia[] {}, apuracao);
+		Assert.assertEquals(new LocalTime(8, 0, 0), apuracao.getHorasTrabalhadas());
+		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasExcedentes());
+		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasFaltantes());
+		// Não deve ter ocorrido nenhuma aprovação:
+		Assert.assertNull(apuracao.getHorasAbonadas());
+		Assert.assertNull(apuracao.getDataAprovacao());
+		Assert.assertNull(apuracao.getResponsavelAprovacao());
+	}
+
+	@Test
+	public void testApuracao_4_OcorrenciaMarcacoesForaPadrao_3() {
+		Apuracao apuracao = new Apuracao();
+		apuracao.setColaborador(colaborador);
+		apuracao.setData(new LocalDate(2014, 2, 6));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(7, 50, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(12, 0, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(13, 0, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(17, 10, 0)));
+
+		apurador.processarApuracao(apuracao);
+
+		Assert.assertEquals(true, apuracao.getApurada());
+		assertOcorrencias(new Ocorrencia[] { Ocorrencia.MARCACOES_FORA_DA_ESCALA }, apuracao);
+		Assert.assertEquals(new LocalTime(8, 0, 0), apuracao.getHorasTrabalhadas());
+		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasExcedentes());
+		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasFaltantes());
+		// Não deve ter ocorrido nenhuma aprovação:
+		Assert.assertNull(apuracao.getHorasAbonadas());
+		Assert.assertNull(apuracao.getDataAprovacao());
+		Assert.assertNull(apuracao.getResponsavelAprovacao());
+	}
+
+	@Test
+	public void testApuracao_5_OcorrenciaMarcacoesForaPadrao_4() {
+		Apuracao apuracao = new Apuracao();
+		apuracao.setColaborador(colaborador);
+		apuracao.setData(new LocalDate(2014, 2, 6));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(8, 10, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(12, 0, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(13, 0, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(16, 50, 0)));
+
+		apurador.processarApuracao(apuracao);
+
+		Assert.assertEquals(true, apuracao.getApurada());
+		assertOcorrencias(new Ocorrencia[] { Ocorrencia.MARCACOES_FORA_DA_ESCALA }, apuracao);
+		Assert.assertEquals(new LocalTime(8, 0, 0), apuracao.getHorasTrabalhadas());
+		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasExcedentes());
+		Assert.assertEquals(new LocalTime(0, 0, 0), apuracao.getHorasFaltantes());
+		// Não deve ter ocorrido nenhuma aprovação:
+		Assert.assertNull(apuracao.getHorasAbonadas());
+		Assert.assertNull(apuracao.getDataAprovacao());
+		Assert.assertNull(apuracao.getResponsavelAprovacao());
+	}
+
+	@Test
+	public void testApuracao_6_OcorrenciaMarcacoesExcedentes() {
+		Apuracao apuracao = new Apuracao();
+		apuracao.setColaborador(colaborador);
+		apuracao.setData(new LocalDate(2014, 2, 6));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(7, 59, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(8, 0, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(12, 0, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(13, 0, 0)));
+		apuracao.addMarcacao(new Marcacao(new LocalTime(17, 0, 0)));
+
+		apurador.processarApuracao(apuracao);
+
+		Assert.assertEquals(true, apuracao.getApurada());
+		assertOcorrencias(new Ocorrencia[] { Ocorrencia.MARCACOES_EXCEDENTES }, apuracao);
+		// Não deve-se calcular as horas quando existe número ímpar de marcações:
+		Assert.assertEquals(null, apuracao.getHorasTrabalhadas());
+		Assert.assertEquals(null, apuracao.getHorasExcedentes());
+		Assert.assertEquals(null, apuracao.getHorasFaltantes());
+		// Não deve ter ocorrido nenhuma aprovação:
+		Assert.assertNull(apuracao.getHorasAbonadas());
+		Assert.assertNull(apuracao.getDataAprovacao());
+		Assert.assertNull(apuracao.getResponsavelAprovacao());
+	}
+
 	// Cenários Daniel:
 
 }
