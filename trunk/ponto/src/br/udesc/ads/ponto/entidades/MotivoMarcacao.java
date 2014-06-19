fcbc24860593a597d91ca2113caea857f3a948d8
anderson.dkfachini@gmail.com <anderson.dkfachini@gmail.com@e197c4ee-6bdd-a554-de2f-b516fb6a7f70>
2014-06-19 22:12:18 +0000

    Implementada a tela de manutenção de motivos de ajuste de ponto

--- trunk/ponto/src/br/udesc/ads/ponto/entidades/MotivoMarcacao.java
+++ trunk/ponto/src/br/udesc/ads/ponto/entidades/MotivoMarcacao.java
@@ -1,17 +1,26 @@
 package br.udesc.ads.ponto.entidades;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class MotivoMarcacao {
+public class MotivoMarcacao implements Serializable {
+
+	private static final long serialVersionUID = -7914410422388107820L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
 	private String descricao;
+	
+	@Column(columnDefinition = "CHAR(1)")
+	private Situacao situacao;
 
 	public Long getId() {
 		return id;
@@ -27,6 +36,14 @@ public class MotivoMarcacao {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public Situacao getSituacao() {
+		return situacao;
+	}
+
+	public void setSituacao(Situacao situacao) {
+		this.situacao = situacao;
 	}
 
 }
