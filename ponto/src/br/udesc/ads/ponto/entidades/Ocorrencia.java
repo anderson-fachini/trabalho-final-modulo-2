54854f414611249fc70837cbd38074172e281b12
anderson.dkfachini@gmail.com <anderson.dkfachini@gmail.com@e197c4ee-6bdd-a554-de2f-b516fb6a7f70>
2014-06-20 17:12:13 +0000

    Externalizadas strings da classe Ocorrencia

--- ponto/src/br/udesc/ads/ponto/entidades/Ocorrencia.java
+++ ponto/src/br/udesc/ads/ponto/entidades/Ocorrencia.java
@@ -1,16 +1,18 @@
 package br.udesc.ads.ponto.entidades;
 
+import br.udesc.ads.ponto.util.Messages;
+
 public enum Ocorrencia {
 
-	HORAS_EXCEDENTES(1, "Horas excedentes"), //
-	HORAS_FALTANTES(2, "Horas faltantes"), //
-	MARCACOES_FORA_DA_ESCALA(3, "Marcações fora da escala"), //
-	MARCACOES_FALTANTES(4, "Marcações faltantes"), //
-	MARCACOES_EXCEDENTES(5, "Marcações excedentes"), //
-	INTERVALO_ALMOCO_INCOMPLETO(6, "Intervalo de almoço incompleto"), //
-	INTERVALO_INTERJORNADAS_INCOMPLETO(7, "Intervalo interjornadas incompleto"), //
-	INTERVALO_INTRAJORNADA_INCOMPLETO(8, "Intervalo intrajornada incompleto"), //
-	INTERVALO_TRABALHO_EXCEDIDO(9, "Intervalo de trabalho excedido");
+	HORAS_EXCEDENTES(1, Messages.getString("horasExcedentes")), //
+	HORAS_FALTANTES(2, Messages.getString("horasFaltantes")), //
+	MARCACOES_FORA_DA_ESCALA(3, Messages.getString("marcacoesForaEscala")), //
+	MARCACOES_FALTANTES(4, Messages.getString("marcacoesFaltantes")), //
+	MARCACOES_EXCEDENTES(5, Messages.getString("marcacoesExcedentes")), //
+	INTERVALO_ALMOCO_INCOMPLETO(6, Messages.getString("intervaloAlmocoIncompleto")), //
+	INTERVALO_INTERJORNADAS_INCOMPLETO(7, Messages.getString("intervaloInterjornadasIncompleto")), //
+	INTERVALO_INTRAJORNADA_INCOMPLETO(8, Messages.getString("intervaloIntrajornadasIncompleto")), //
+	INTERVALO_TRABALHO_EXCEDIDO(9, Messages.getString("intervaloTrabalhoExcedido"));
 
 	private int id;
 	private String descricao;
