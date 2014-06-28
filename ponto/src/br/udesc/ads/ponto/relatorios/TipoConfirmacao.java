db1c51a0a52136caecc01c93329259ac1027b8d2
samueldeschamps@gmail.com <samueldeschamps@gmail.com@e197c4ee-6bdd-a554-de2f-b516fb6a7f70>
2014-06-28 00:22:43 +0000

    Ignorando algumas Ocorrencias quando o dia é feriado ou final de semana.

--- ponto/src/br/udesc/ads/ponto/relatorios/TipoConfirmacao.java
+++ ponto/src/br/udesc/ads/ponto/relatorios/TipoConfirmacao.java
@@ -4,20 +4,20 @@ import br.udesc.ads.ponto.util.Messages;
 
 public enum TipoConfirmacao {
 
+	TODAS(Messages.getString("todas")), //
 	CONFIRMADAS(Messages.getString("confirmadas")), //
-	PENDENTES(Messages.getString("pendentes")), //
-	TODAS(Messages.getString("todas"));
-	
+	PENDENTES(Messages.getString("pendentes"));
+
 	private String descricao;
 
 	private TipoConfirmacao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
 	public String getDescricao() {
 		return descricao;
 	}
-	
+
 	@Override
 	public String toString() {
 		return getDescricao();
